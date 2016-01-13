204476a306bfc8690f9acc1b57ac4fbbf257fcf3
François Gaillard <gailla.fr@gmail.com>
2016-01-13 10:32:29 +0100

    // Happy New Year PrestaShop!

--- backwardcompatibility.php
+++ backwardcompatibility.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* 2007-2015 PrestaShop
+* 2007-2016 PrestaShop
 *
 * NOTICE OF LICENSE
 *
@@ -19,7 +19,7 @@
 * needs please refer to http://www.prestashop.com for more information.
 *
 *  @author PrestaShop SA <contact@prestashop.com>
-*  @copyright  2007-2015 PrestaShop SA
+*  @copyright  2007-2016 PrestaShop SA
 *  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
@@ -45,12 +45,12 @@ class BackwardCompatibility extends Module
 		if ($this->active && defined('_PS_ADMIN_DIR_'))
 			$this->addContext();
 	}
-	
+
 	public function install()
 	{
 		if (version_compare(_PS_VERSION_, '1.5') >= 0)
 			return false;
-		
+
 		return parent::install();
 	}
 
@@ -161,5 +161,3 @@ class BackwardCompatibility extends Module
 		return false;
 	}
 }
-
-
